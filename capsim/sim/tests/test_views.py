ff5581bd87aeffcc50251db620d74ef6541b7990
Anders Pearson <anders@columbia.edu>
2013-09-20 16:37:13 -0400

    move the template to the matching location

--- capsim/sim/tests/test_views.py
+++ capsim/sim/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.test.client import Client
+from capsim.sim.models import RunRecord
 
 
 class BasicViewTest(TestCase):
@@ -7,6 +8,12 @@ class BasicViewTest(TestCase):
         self.c = Client()
 
     def test_runs(self):
-        response = self.c.get("/runs/")
+        response = self.c.get("/run/")
         self.assertEquals(response.status_code, 200)
         self.assertTrue("<ul>" in response.content)
+
+    def test_run(self):
+        rr = RunRecord.objects.create()
+        response = self.c.get("/run/%d/" % rr.id)
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue("<h2>Saved Run: " in response.content)
--- capsim/sim/views.py
+++ capsim/sim/views.py
@@ -1,7 +1,14 @@
 from annoying.decorators import render_to
+from django.shortcuts import get_object_or_404
 from capsim.sim.models import RunRecord
 
 
-@render_to('main/runs.html')
+@render_to('sim/runs.html')
 def runs(request):
     return dict(runs=RunRecord.objects.all())
+
+
+@render_to('sim/run.html')
+def run(request, id):
+    r = get_object_or_404(RunRecord, id=id)
+    return dict(run=r)
