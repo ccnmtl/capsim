69cdb86f80fa3a7da8ed62c19bbb94ac186893af
Anders Pearson <anders@columbia.edu>
2014-01-23 16:53:23 -0500

    need django's messages middleware

--- capsim/settings_shared.py
+++ capsim/settings_shared.py
@@ -80,6 +80,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'django.middleware.transaction.TransactionMiddleware',
     'impersonate.middleware.ImpersonateMiddleware',
