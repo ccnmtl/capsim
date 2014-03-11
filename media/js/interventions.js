b913573596ed22bcca38373feb299fa9dca85c8b
Anders Pearson <anders@columbia.edu>
2014-03-11 12:10:57 -0400

    rough start to parameter adjustment via interventions

--- media/js/interventions.js
+++ media/js/interventions.js
@@ -39,8 +39,81 @@ var calculate_budget = function() {
             budget_used += cost;
         }
     }
-
+    apply_modifiers();
     update_budget_progress_bar();
+};
+
+var defaults = {
+    'id_recreation_activity_alpha': 0.5,
+    'id_education_activity_alpha': 0.5,
+    'id_domestic_activity_alpha': 0.5,
+    'id_transport_activity_alpha': 0.5,
+    'id_food_convenience_alpha': 0.5,
+    'id_energy_density_alpha': 0.5,
+    'id_food_exposure_alpha': 0.5,
+    'id_food_convenience_alpha': 0.5,
+    'id_food_literacy_alpha': 0.5,
+    'id_food_advertising_alpha': 0.5,
+    'id_gamma_6': 1.0,
+}
+
+var modifiers = {
+    'increase-physical-activity': {
+        'high': [{'param': 'id_recreation_activity_alpha', 'adjustment': 0.3}],
+        'medium': [{'param': 'id_recreation_activity_alpha', 'adjustment': 0.2}],
+        'low': [{'param': 'id_recreation_activity_alpha', 'adjustment': 0.1}]
+    },
+    'ensure-screening': {
+        'high': [],
+        'medium': [],
+        'low': []
+    },
+    'active-living-at-work': {
+        'high': [{'param': 'id_domestic_activity_alpha', 'adjustment': 0.3}],
+        'medium': [{'param': 'id_domestic_activity_alpha', 'adjustment': 0.2}],
+        'low': [{'param': 'id_domestic_activity_alpha', 'adjustment': 0.1}]
+    },
+    'social-influence': {
+        'high': [{'param': 'id_gamma_6', 'adjustment': 0.3}],
+        'medium': [{'param': 'id_gamma_6', 'adjustment': 0.2}],
+        'low': [{'param': 'id_gamma_6', 'adjustment': 0.1}]
+    },
+    'physical-environment': {
+        'high': [],
+        'medium': [],
+        'low': []
+    }
+}
+
+var current_values = {};
+
+var reset_parameters = function() {
+    for (var k in defaults) {
+        if (defaults.hasOwnProperty(k)) {
+            $("#" + k).val(defaults[k]);
+            current_values[k] = defaults[k];
+        }
+    }
+};
+
+var apply_modifiers = function() {
+    // set everything to a known state
+    reset_parameters();
+    // then go through each control that is set and adjust accordingly
+    var controls = $(".intervention-control");
+    for (var i=0; i < controls.length; i++) {
+        if (controls[i].value != "") {
+            var m = modifiers[controls[i].id][controls[i].value];
+            for (var j=0; j < m.length; j++) {
+                var param_name = m[j].param;
+                var adjustment = m[j].adjustment;
+                var p = $("#" + param_name);
+                var new_value = current_values[param_name] + adjustment;
+                current_values[param_name] = new_value;
+                p.val(new_value);
+            }
+        }
+    }
 };
 
 $(".intervention-control").change(calculate_budget);
