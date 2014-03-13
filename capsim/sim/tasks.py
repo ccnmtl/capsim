e05a063a22b1f06e628c0ca4c1c5411abc23e081
Anders Pearson <anders@columbia.edu>
2014-03-13 16:42:39 -0400

    statsd counter for tasks

--- capsim/sim/tasks.py
+++ capsim/sim/tasks.py
@@ -1,9 +1,11 @@
 from celery.decorators import task
+from django_statsd.clients import statsd
 from .models import Experiment, ExpRun, RunRecord, RunOutputRecord
 
 
 @task
 def run_experiment(experiment_id):
+    statsd.incr("run_experiment")
     print "running experiment %d" % experiment_id
     e = Experiment.objects.get(id=experiment_id)
     e.populate(callback=process_run.delay)
@@ -11,6 +13,7 @@ def run_experiment(experiment_id):
 
 @task
 def process_run(run_id, exprun_id):
+    statsd.incr("process_run")
     print "process_run %d, %d" % (run_id, exprun_id)
     rr = RunRecord.objects.get(id=run_id)
     r = rr.get_run()
