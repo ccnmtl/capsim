8e53e2709952be97208d4ad40a4d1fe153093d12
Anders Pearson <anders@columbia.edu>
2014-04-29 11:23:01 -0400

    google analytics support

--- capsim/settings_shared.py
+++ capsim/settings_shared.py
@@ -73,6 +73,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
     'django.core.context_processors.request',
     'stagingcontext.staging_processor',
+    'gacontext.ga_processor',
     'django.core.context_processors.static',
     'djangowind.context.context_processor',
 )
