c85cecd175d78310d97a010448b5876acd9a42af
Anders Pearson <anders@columbia.edu>
2015-02-06 14:31:56 +0100

    label graphs and axis. PMT #99205

--- media/js/graph.js
+++ media/js/graph.js
@@ -1,73 +1,52 @@
 //var margin = 15;
 //width = 860;
 //height = 200;
-var makeGraph = function (loc, data, width, height, margin) {
-    var w = width;
-    var h = height;
+var makeGraph = function (loc, data, width, height, yAxisLabel) {
+    var margin = {top: 30, right: 20, bottom: 30, left: 50};
+    var w = width - margin.left - margin.right;
+    var h = height - margin.top - margin.bottom;
 
-    y = d3.scale.linear().domain([0, d3.max(data)]).range([0 + margin, height - margin]);
-    x = d3.scale.linear().domain([0, data.length]).range([0 + margin, width - margin]);
+    y = d3.scale.linear().domain([120, 80]).range([0, h]);
+    x = d3.scale.linear().domain([0, data.length]).range([0, w]);
 
-    var vis = d3.select(loc)
-        .append("svg:svg")
-        .attr("width", w)
-        .attr("height", h);
-
-    var g = vis.append("svg:g")
-        .attr("transform", "translate(0, " + height + ")");
-		
-    var line = d3.svg.line()
+    var xAxis = d3.svg.axis().scale(x)
+         .orient("bottom").ticks(5);
+    var yAxis = d3.svg.axis().scale(y)
+         .orient("left").ticks(5);
+    var valueLine = d3.svg.line()
 		    .x(function(d,i) { return x(i); })
-		    .y(function(d) { return -1 * y(d); });
+		    .y(function(d) { return y(d); });
 		
-    g.append("svg:path").attr("d", line(data)).attr("class", "mean");
-		
-    g.append("svg:line")
-		    .attr("x1", x(0))
-		    .attr("y1", -1 * y(0))
-		    .attr("x2", x(w))
-		    .attr("y2", -1 * y(0));
+    var svg = d3.select(loc)
+        .append("svg:svg")
+            .attr("width", width)
+            .attr("height", height)
+        .append("svg:g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    g.append("svg:line")
-		    .attr("x1", x(0))
-		    .attr("y1", -1 * y(0))
-		    .attr("x2", x(0))
-		    .attr("y2", -1 * y(d3.max(data)));
-		
-    g.selectAll(".xLabel")
-		    .data(x.ticks(5))
-		    .enter().append("svg:text")
-		    .attr("class", "xLabel")
-		    .text(String)
-		    .attr("x", function(d) { return x(d) })
-		    .attr("y", 0)
-		    .attr("text-anchor", "middle");
+    svg.append("path")
+       .attr("d", valueLine(data))
+       .attr("class", "mean");
 
-    g.selectAll(".yLabel")
-		    .data(y.ticks(4))
-		    .enter().append("svg:text")
-		    .attr("class", "yLabel")
-		    .text(String)
-		    .attr("x", 0)
-		    .attr("y", function(d) { return -1 * y(d) })
-		    .attr("text-anchor", "right")
-		    .attr("dy", 4);
-		
-    g.selectAll(".xTicks")
-		    .data(x.ticks(5))
-		    .enter().append("svg:line")
-		    .attr("class", "xTicks")
-		    .attr("x1", function(d) { return x(d); })
-		    .attr("y1", -1 * y(0))
-		    .attr("x2", function(d) { return x(d); })
-		    .attr("y2", -1 * y(-0.3));
+    svg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + h + ")")
+        .call(xAxis);
 
-    g.selectAll(".yTicks")
-		    .data(y.ticks(4))
-		    .enter().append("svg:line")
-		    .attr("class", "yTicks")
-		    .attr("y1", function(d) { return -1 * y(d); })
-		    .attr("x1", x(-0.3))
-		    .attr("y2", function(d) { return -1 * y(d); })
-		    .attr("x2", x(0));
+    svg.append("text")
+        .attr("transform", "translate(" + (w / 2) + " ," + (h + margin.bottom) + ")")
+        .style("text-anchor", "middle")
+        .text("Simulation Time");
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (h / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text(yAxisLabel);
 };
