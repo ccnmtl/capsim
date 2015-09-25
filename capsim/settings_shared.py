263dedadc5a08712bb14c6333ab0076af355e147
Anders Pearson <anders@columbia.edu>
2015-09-25 09:08:53 +0200

    ccnmtlsettings

--- capsim/settings_shared.py
+++ capsim/settings_shared.py
@@ -1,121 +1,31 @@
 # Django settings for capsim project.
 import os.path
-import sys
 import djcelery
+import sys
+from ccnmtlsettings.shared import common
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+project = 'capsim'
+base = os.path.dirname(__file__)
 
-ADMINS = ()
+locals().update(common(project=project, base=base))
 
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'capsim',
-        'HOST': '',
-        'PORT': 5432,
-        'USER': '',
-        'PASSWORD': '',
-        'ATOMIC_REQUESTS': True,
-    }
-}
-
-if 'test' in sys.argv or 'jenkins' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-            'HOST': '',
-            'PORT': '',
-            'USER': '',
-            'PASSWORD': '',
-            'ATOMIC_REQUESTS': True,
-        }
-    }
-    CELERY_ALWAYS_EAGER = True
-    BROKER_BACKEND = 'memory'
-
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pep8',
-)
 PROJECT_APPS = [
     'capsim.main', 'capsim.sim'
 ]
 
-ALLOWED_HOSTS = ['localhost', '.ccnmtl.columbia.edu']
-
 USE_TZ = True
-TIME_ZONE = 'America/New_York'
-LANGUAGE_CODE = 'en-us'
-SITE_ID = 1
-USE_I18N = False
-MEDIA_ROOT = "/var/www/capsim/uploads/"
-MEDIA_URL = '/uploads/'
-STATIC_URL = '/media/'
-SECRET_KEY = ')ng#)ef_u@_^zvvu@dxm7ql-yb^_!a6%v3v^j3b(mp+)l+5%@h'
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.template.context_processors.debug',
-    'django.template.context_processors.request',
-    'django.template.context_processors.static',
-    'django.template.context_processors.media',
-    'stagingcontext.staging_processor',
-    'gacontext.ga_processor',
-    'djangowind.context.context_processor',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'impersonate.middleware.ImpersonateMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'waffle.middleware.WaffleMiddleware',
-)
-
-ROOT_URLCONF = 'capsim.urls'
-
-TEMPLATE_DIRS = (
-    "/var/www/capsim/templates/",
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
 
 djcelery.setup_loader()
 
-INSTALLED_APPS = [
-    'gunicorn',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    'django_markwhat',
-    'django.contrib.staticfiles',
-    'django.contrib.messages',
-    'django.contrib.admin',
+if 'test' in sys.argv or 'jenkins' in sys.argv:
+    CELERY_ALWAYS_EAGER = True
+    BROKER_BACKEND = 'memory'
+
+
+INSTALLED_APPS += [  # noqa
     'django.contrib.humanize',
-    'compressor',
-    'django_statsd',
     'bootstrapform',
-    'debug_toolbar',
-    'waffle',
-    'django_jenkins',
-    'smoketest',
     'django_extensions',
-    'impersonate',
     'pagetree',
     'pageblocks',
     'quizblock',
@@ -137,67 +47,9 @@ PAGEBLOCKS = ['pageblocks.TextBlock',
 BROKER_URL = "amqp://localhost:5672//dmt"
 CELERYD_CONCURRENCY = 4
 
-INTERNAL_IPS = ('127.0.0.1', )
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-)
-
 IPYTHON_ARGUMENTS = [
     '--pylab',
 ]
-
-STATSD_CLIENT = 'statsd.client'
-STATSD_PREFIX = 'capsim'
-STATSD_HOST = '127.0.0.1'
-STATSD_PORT = 8125
-
-THUMBNAIL_SUBDIR = "thumbs"
-EMAIL_SUBJECT_PREFIX = "[capsim] "
-EMAIL_HOST = 'localhost'
-SERVER_EMAIL = "capsim@ccnmtl.columbia.edu"
-DEFAULT_FROM_EMAIL = SERVER_EMAIL
-
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), "../media")
-STATICFILES_DIRS = (
-)
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-
-COMPRESS_URL = "/media/"
-COMPRESS_ROOT = "media/"
-
-# CAS settings
-AUTHENTICATION_BACKENDS = ('djangowind.auth.SAMLAuthBackend',
-                           'django.contrib.auth.backends.ModelBackend', )
-CAS_BASE = "https://cas.columbia.edu/"
-WIND_PROFILE_HANDLERS = ['djangowind.auth.CDAPProfileHandler']
-WIND_AFFIL_HANDLERS = ['djangowind.auth.AffilGroupMapper',
-                       'djangowind.auth.StaffMapper',
-                       'djangowind.auth.SuperuserMapper']
-WIND_STAFF_MAPPER_GROUPS = ['tlc.cunix.local:columbia.edu']
-WIND_SUPERUSER_MAPPER_GROUPS = ['anp8', 'jb2410', 'zm4', 'egr2107',
-                                'sld2131', 'amm8', 'mar227']
-
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_HTTPONLY = True
-LOGIN_REDIRECT_URL = "/"
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-}
 
 INTERVENTION_SKEWS = {
     # 1-2: Provide and Support Community Programs Designed
