370507026d32f79a914be001234434529b5b2363
Anders Pearson <anders@columbia.edu>
2016-08-02 15:28:27 +0100

    eliminate django.conf.urls.patterns
    deprecated in Django 1.9, removed in 1.10

--- capsim/urls.py
+++ capsim/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.views.generic import TemplateView
@@ -12,95 +12,95 @@ from capsim.sim.models import RunRecord, Experiment, Parameter
 admin.autodiscover()
 
 redirect_after_logout = getattr(settings, 'LOGOUT_REDIRECT_URL', None)
-auth_urls = (r'^accounts/', include('django.contrib.auth.urls'))
-logout_page = (
+auth_urls = url(r'^accounts/', include('django.contrib.auth.urls'))
+logout_page = url(
     r'^accounts/logout/$',
     'django.contrib.auth.views.logout',
     {'next_page': redirect_after_logout})
 if hasattr(settings, 'CAS_BASE'):
-    auth_urls = (r'^accounts/', include('djangowind.urls'))
-    logout_page = (
+    auth_urls = url(r'^accounts/', include('djangowind.urls'))
+    logout_page = url(
         r'^accounts/logout/$',
         'djangowind.views.logout',
         {'next_page': redirect_after_logout})
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     auth_urls,
     logout_page,
-    (r'^$', TemplateView.as_view(template_name="main/home.html")),
-    (r'^contact/$', TemplateView.as_view(template_name="main/contact.html")),
-    (r'^about/$', TemplateView.as_view(template_name="main/about.html")),
-    (r'^topics/$', TemplateView.as_view(template_name="main/topics.html")),
-    (r'^topic-obesity/$', TemplateView.as_view(
+    url(r'^$', TemplateView.as_view(template_name="main/home.html")),
+    url(r'^contact/$', TemplateView.as_view(
+        template_name="main/contact.html")),
+    url(r'^about/$', TemplateView.as_view(template_name="main/about.html")),
+    url(r'^topics/$', TemplateView.as_view(template_name="main/topics.html")),
+    url(r'^topic-obesity/$', TemplateView.as_view(
         template_name="sim/topic-obesity.html")),
-    (r'^topic-obesity-debrief/$', TemplateView.as_view(
+    url(r'^topic-obesity-debrief/$', TemplateView.as_view(
         template_name="sim/topic-obesity-debrief.html")),
-    (r'^proposal/$', TemplateView.as_view(
+    url(r'^proposal/$', TemplateView.as_view(
         template_name="sim/proposal.html")),
-    (r'^run/new/$', capsim.main.views.NewRunView.as_view()),
-    (r'^run/$', capsim.sim.views.RunsView.as_view()),
-    (r'^run/compare/$', capsim.sim.views.CompareRunsView.as_view()),
-    (r'^run/toggle/$', capsim.main.views.ToggleFlagView.as_view()),
-    (r'^run/(?P<id>\d+)/$', capsim.sim.views.RunView.as_view()),
-    (r'^run/(?P<pk>\d+)/json/$', capsim.sim.views.RunOutputView.as_view()),
-    (r'^run/(?P<pk>\d+)/edit/$', capsim.sim.views.RunEditView.as_view()),
-    (r'^run/(?P<pk>\d+)/delete/$',
-     DeleteView.as_view(model=RunRecord, success_url="/run/")),
+    url(r'^run/new/$', capsim.main.views.NewRunView.as_view()),
+    url(r'^run/$', capsim.sim.views.RunsView.as_view()),
+    url(r'^run/compare/$', capsim.sim.views.CompareRunsView.as_view()),
+    url(r'^run/toggle/$', capsim.main.views.ToggleFlagView.as_view()),
+    url(r'^run/(?P<id>\d+)/$', capsim.sim.views.RunView.as_view()),
+    url(r'^run/(?P<pk>\d+)/json/$', capsim.sim.views.RunOutputView.as_view()),
+    url(r'^run/(?P<pk>\d+)/edit/$', capsim.sim.views.RunEditView.as_view()),
+    url(r'^run/(?P<pk>\d+)/delete/$',
+        DeleteView.as_view(model=RunRecord, success_url="/run/")),
 
-    (r'^experiment/new/$', capsim.sim.views.NewExperimentView.as_view()),
-    (r'^experiment/$', capsim.sim.views.ExperimentListView.as_view()),
-    (r'^experiment/(?P<pk>\d+)/$', DetailView.as_view(model=Experiment)),
-    (r'^experiment/(?P<pk>\d+)/csv/$',
-     capsim.sim.views.ExperimentOutputView.as_view()),
-    (r'^experiment/(?P<pk>\d+)/fullcsv/$',
-     capsim.sim.views.ExperimentFullOutputView.as_view()),
-    (r'^experiment/(?P<pk>\d+)/heatmap/$',
-     DetailView.as_view(
-         model=Experiment,
-         template_name="sim/experiment_heatmap.html")),
-    (r'^experiment/(?P<pk>\d+)/delete/$',
-     capsim.sim.views.ExperimentDeleteView.as_view()),
-    (r'^experiment/(?P<pk>\d+)/reenqueue/$',
-     capsim.sim.views.ExperimentReEnqueueView.as_view()),
+    url(r'^experiment/new/$', capsim.sim.views.NewExperimentView.as_view()),
+    url(r'^experiment/$', capsim.sim.views.ExperimentListView.as_view()),
+    url(r'^experiment/(?P<pk>\d+)/$', DetailView.as_view(model=Experiment)),
+    url(r'^experiment/(?P<pk>\d+)/csv/$',
+        capsim.sim.views.ExperimentOutputView.as_view()),
+    url(r'^experiment/(?P<pk>\d+)/fullcsv/$',
+        capsim.sim.views.ExperimentFullOutputView.as_view()),
+    url(r'^experiment/(?P<pk>\d+)/heatmap/$',
+        DetailView.as_view(
+            model=Experiment,
+            template_name="sim/experiment_heatmap.html")),
+    url(r'^experiment/(?P<pk>\d+)/delete/$',
+        capsim.sim.views.ExperimentDeleteView.as_view()),
+    url(r'^experiment/(?P<pk>\d+)/reenqueue/$',
+        capsim.sim.views.ExperimentReEnqueueView.as_view()),
 
-    (r'^calibrate/$', TemplateView.as_view(
+    url(r'^calibrate/$', TemplateView.as_view(
         template_name="sim/calibrate_index.html")),
-    (r'^calibrate/intervention/$',
-     capsim.sim.views.InterventionListView.as_view()),
-    (r'^calibrate/intervention/(?P<pk>\d+)/$',
-     capsim.sim.views.InterventionEditView.as_view()),
-    (r'^calibrate/intervention/add/$',
-     capsim.sim.views.InterventionAddView.as_view()),
-    (r'^calibrate/intervention/set_costs/$',
-     capsim.sim.views.InterventionSetCostsView.as_view()),
+    url(r'^calibrate/intervention/$',
+        capsim.sim.views.InterventionListView.as_view()),
+    url(r'^calibrate/intervention/(?P<pk>\d+)/$',
+        capsim.sim.views.InterventionEditView.as_view()),
+    url(r'^calibrate/intervention/add/$',
+        capsim.sim.views.InterventionAddView.as_view()),
+    url(r'^calibrate/intervention/set_costs/$',
+        capsim.sim.views.InterventionSetCostsView.as_view()),
 
-    (r'^calibrate/parameter/$',
-     capsim.sim.views.ParameterListView.as_view()),
-    (r'^calibrate/parameter/(?P<pk>\d+)/$',
-     capsim.sim.views.ParameterEditView.as_view()),
-    (r'^calibrate/parameter/add/$',
-     capsim.sim.views.ParameterAddView.as_view()),
-    (r'^calibrate/parameter/(?P<pk>\d+)/delete/$',
-     DeleteView.as_view(
-         model=Parameter,
-         success_url="/calibrate/parameter/")),
+    url(r'^calibrate/parameter/$',
+        capsim.sim.views.ParameterListView.as_view()),
+    url(r'^calibrate/parameter/(?P<pk>\d+)/$',
+        capsim.sim.views.ParameterEditView.as_view()),
+    url(r'^calibrate/parameter/add/$',
+        capsim.sim.views.ParameterAddView.as_view()),
+    url(r'^calibrate/parameter/(?P<pk>\d+)/delete/$',
+        DeleteView.as_view(
+            model=Parameter,
+            success_url="/calibrate/parameter/")),
 
-    (r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^_impersonate/', include('impersonate.urls')),
-    (r'^stats/', TemplateView.as_view(template_name="stats.html")),
-    (r'^model/', TemplateView.as_view(template_name="main/model.html")),
-    (r'smoketest/', include('smoketest.urls')),
-    (r'^infranil/(?P<path>.*)$', InfranilView.as_view()),
-    (r'^uploads/(?P<path>.*)$',
-     'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    (r'^pagetree/', include('pagetree.urls')),
-    (r'^quizblock/', include('quizblock.urls')),
-    (r'^pages/edit/(?P<path>.*)$', capsim.main.views.EditPage.as_view(),
-     {}, 'edit-page'),
-    (r'^pages/instructor/(?P<path>.*)$',
-     capsim.main.views.InstructorPage.as_view()),
-    (r'^pages/(?P<path>.*)$', PageView.as_view(
+    url(r'^stats/', TemplateView.as_view(template_name="stats.html")),
+    url(r'^model/', TemplateView.as_view(template_name="main/model.html")),
+    url(r'smoketest/', include('smoketest.urls')),
+    url(r'^infranil/(?P<path>.*)$', InfranilView.as_view()),
+    url(r'^uploads/(?P<path>.*)$',
+        'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'^pagetree/', include('pagetree.urls')),
+    url(r'^quizblock/', include('quizblock.urls')),
+    url(r'^pages/edit/(?P<path>.*)$', capsim.main.views.EditPage.as_view(),
+        {}, 'edit-page'),
+    url(r'^pages/instructor/(?P<path>.*)$',
+        capsim.main.views.InstructorPage.as_view()),
+    url(r'^pages/(?P<path>.*)$', PageView.as_view(
         hierarchy_name="main",
         hierarchy_base="/pages/")),
-)
+]
