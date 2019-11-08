b108421be99fb20f8bbe6472fd9d87934726ff77
Nick Buonincontri <nbuonin@gmail.com>
2019-11-08 13:04:45 -0500

    Adjust flake8 in settings

--- capsim/settings_production.py
+++ capsim/settings_production.py
@@ -1,5 +1,4 @@
-# flake8: noqa
-from capsim.settings_shared import *
+from capsim.settings_shared import *  # noqa F403
 from ccnmtlsettings.production import common
 from django.conf import settings
 import sentry_sdk
@@ -7,20 +6,20 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 locals().update(
     common(
-        project=project,
-        base=base,
-        STATIC_ROOT=STATIC_ROOT,
-        INSTALLED_APPS=INSTALLED_APPS,
+        project=project,  # noqa F405
+        base=base,  # noqa F405
+        STATIC_ROOT=STATIC_ROOT,  # noqa F405
+        INSTALLED_APPS=INSTALLED_APPS,  # noqa F405
         cloudfront="d1xa4pq0x5fm1x",
     ))
 
 try:
-    from capsim.local_settings import *
+    from capsim.local_settings import *  # noqa F403
 except ImportError:
     pass
 
 if hasattr(settings, 'SENTRY_DSN'):
     sentry_sdk.init(
-        dsn=SENTRY_DSN,
+        dsn=SENTRY_DSN,  # noqa F405
         integrations=[DjangoIntegration()],
     )
